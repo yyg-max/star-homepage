bafad7a5802024e0272246026fa930738581926c
yyg-max <yan275947@gmail.com>
2024-11-01 20:17:34 +0800

    Update TheFooter.tsx

--- src/components/TheFooter/TheFooter.tsx
+++ src/components/TheFooter/TheFooter.tsx
@@ -5,17 +5,17 @@ import styles from './styles.module.scss'
 
 export default defineComponent({
   name: 'TheFooter',
-  setup() {
-    return () => (
-      // <div class={styles.footer}>
-      //   <div>
-      //     DESIGN BY <a href="https://github.com/aweikalee">@AWEIKALEE (毛呆)</a>
-      //   </div>
+  // setup() {
+  //   return () => (
+  //     <div class={styles.footer}>
+  //       <div>
+  //         DESIGN BY <a href="https://github.com/aweikalee">@AWEIKALEE (毛呆)</a>
+  //       </div>
 
-      //   <div>
-      //     <a href={beian.url}>{beian.text}</a>
-      //   </div>
-      // </div>
-    )
-  },
+  //       <div>
+  //         <a href={beian.url}>{beian.text}</a>
+  //       </div>
+  //     </div>
+  //   )
+  // },
 })
